3499daeee8a5b97941350bad83b9326f0a493e5d
Franco Reyes <fr.reyesh@duocuc.cl>
2025-05-26 01:41:52 -0400

    name changes

--- src/test/java/com/logistica/orm4/LogisticaApplicationTests.java
+++ src/test/java/com/logistica/orm4/LogisticaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hospital.orm4;
+package com.logistica.orm4;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Orm4ApplicationTests {
+class LogisticaApplicationTests {
 
 	@Test
 	void contextLoads() {
